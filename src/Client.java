dc8c3c0d91691d78516c21d6d003ce69ead56a70
Ezatullah <ezatullahahmadi350@gmail.com>
2025-01-07 12:29:11 +0330

    server and client successfully connected

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,15 @@
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client {
     public static void main(String[] args) {
         int port = 8090;
-
+        String host = "localhost";
+        String username = "admin";
+        try {
+            Socket socket = new Socket(host,port);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
 
@@ -9,6 +10,10 @@ public class Server {
 
         try (ServerSocket serverSocket = new ServerSocket(port)){
             System.out.println("Server listening on port " + port);
+            while(true){
+                Socket socket = serverSocket.accept();
+                System.out.println("New connection from " + socket.getRemoteSocketAddress());
+            }
 
         } catch (IOException e) {
             throw new RuntimeException(e);
